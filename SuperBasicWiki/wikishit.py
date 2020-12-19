b3cf00ded1271fe564d577c9331b6ac9b6846a6d
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-12-19 16:34:45 +0000

    they need to be outside init fair enough

--- SuperBasicWiki/wikishit.py
+++ SuperBasicWiki/wikishit.py
@@ -1,18 +1,17 @@
 from redbot.core import commands # We going super fucking basic
 from redbot.core.bot import Red
 from typing import Optional
-
+DEFAULT_URL_SHIT = "https://en.scpslgame.com/index.php?title="
+MY_WIFE_LEFT_ME = "Main_Page"
 
 
 class wikishite(commands.Cog):
     """Here we fuckin go"""
     def __init__(self, bot: Red):
         self.bot = bot
-        DEFAULT_URL_SHIT = "https://en.scpslgame.com/index.php?title="
-        MY_WIFE_LEFT_ME = "Main_Page"
 
     @commands.command()
-    async def wikishite(self, ctx, *, args = None):
+    async def wiki(self, ctx, *, args = None):
         if args == None:
             URL = DEFAULT_URL_SHIT + MY_WIFE_LEFT_ME
         else:
