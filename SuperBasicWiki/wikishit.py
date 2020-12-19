38768e5c80a0b07a2a22405c6bf80542b037e70b
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-12-19 16:32:04 +0000

    did i forget init

--- SuperBasicWiki/wikishit.py
+++ SuperBasicWiki/wikishit.py
@@ -6,8 +6,10 @@ from typing import Optional
 
 class wikishite(commands.Cog):
     """Here we fuckin go"""
-    DEFAULT_URL_SHIT = "https://en.scpslgame.com/index.php?title="
-    MY_WIFE_LEFT_ME = "Main_Page"
+    def __init__(self, bot: Red):
+        self.bot = bot
+        DEFAULT_URL_SHIT = "https://en.scpslgame.com/index.php?title="
+        MY_WIFE_LEFT_ME = "Main_Page"
 
     @commands.command()
     async def wikishite(self, ctx, *, args = None):
