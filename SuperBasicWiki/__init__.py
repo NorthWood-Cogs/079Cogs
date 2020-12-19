9e0021e6a34dad7eb0da4e49a22b45a1c2ee2ca9
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-12-19 16:30:18 +0000

    the fuck is wrong with me

--- SuperBasicWiki/__init__.py
+++ SuperBasicWiki/__init__.py
@@ -1,8 +1,7 @@
-#Watch this space
 from .wikishit import wikishite
 
 __red_end_user_data_statement__="We store LITERALLY NOTHING. You're fine."
 
 
-async def setup(bot):
+def setup(bot):
     bot.add_cog(wikishite(bot))
