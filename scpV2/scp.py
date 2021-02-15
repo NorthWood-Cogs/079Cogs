9456bee702ff61114ccc41706e44417ae79a010b
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-15 13:52:21 +0000

    Add some sanity

--- scpV2/scp.py
+++ scpV2/scp.py
@@ -18,7 +18,8 @@ class SCP(commands.Cog):
     async def scp(self, ctx, scpID: str):
         """Finds an SCP based on their number. Standard Content warning applies."""
         target = SCPWiki(f'scp-{scpID}')
-        Preview = target.content
+        PreviewS = target.content
+        Preview = PreviewS[:75] + (PreviewS[75:] and '..')
         await ctx.send(f"{Preview}")
         scpEM = discord.Embed(
             titlle=f"{target.title}",
