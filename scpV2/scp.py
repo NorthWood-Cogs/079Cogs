3c51c614b56bd83092d2345df7961694cd98afbd
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-15 15:49:23 +0000

    not a string

--- scpV2/scp.py
+++ scpV2/scp.py
@@ -17,17 +17,17 @@ class SCP(commands.Cog):
     def ColourPicker(self, OClass):
         #Basically just a list of If statements because fuck it
         if OClass.lower == "safe":
-            return "0x2ecc71"
+            return 0x2ecc71
         if OClass.lower == "euclid":
-            return "0xf1c40f"
+            return 0xf1c40f
         if OClass.lower == "keter":
-            return "0xe74c3c"
+            return 0xe74c3c
         if OClass.lower == "thaumiel":
-            return "0x3498db"
+            return 0x3498db
         if OClass.lower == "explained":
-            return "0xe91e63"
+            return 0xe91e63
         else:
-            return "0x99aab5"
+            return 0x99aab5
 
 
     @commands.command()
