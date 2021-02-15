7b002eb2f8fe8892ad7832e7312e409018170db9
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-15 13:26:47 +0000

    Also gonna test a thing

---
+++ scpV2/scp.py
@@ -0,0 +1,23 @@
+import discord
+import pyscp
+from redbot.core import commands
+from typing import Optional
+
+from redbot.core.commands import Cog
+
+
+
+class SCP(commands.Cog):
+    """ SCP Cog that utilises an especially adapted wikidot api"""
+    def __init__(self, bot):
+        self.bot = bot
+        SCPWiki = pyscp.wikidot.Wiki('www.scpwiki.net')
+
+
+    @commands.command()
+    async def scp(self, ctx, scpID: str):
+        """Finds an SCP based on inputted value or common aliases."""
+        target = SCPWiki(f'scp-{scpID}')
+        await ctx.send(f"{target.title} has a url of {target.url}")
+
+
