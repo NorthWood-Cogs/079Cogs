f8a3d8a3c6452d18e143675410d191bada483b82
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-15 14:34:14 +0000

    Time for a social experiment

--- scpV2/scp.py
+++ scpV2/scp.py
@@ -9,7 +9,7 @@ from redbot.core.commands import Cog
 SCPWiki = pyscp.wikidot.Wiki('scp-wiki.wikidot.com')
 ObjectClass = ["Safe", "Euclid", "Keter", "Thaumiel", "Neutralized", "Explained"]
 class SCP(commands.Cog):
-    """ SCP Cog that utilises an especially adapted wikidot api"""
+    """ SCP Cog that utilises an especially adapted wikidot api""" # Their Claim, not mine
     def __init__(self, bot):
         self.bot = bot
 
@@ -18,13 +18,16 @@ class SCP(commands.Cog):
     async def scp(self, ctx, scpID: str):
         """Finds an SCP based on their number. Standard Content warning applies."""
         target = SCPWiki(f'scp-{scpID}')
-        await ctx.send(f"{target.title}")
         BaseContentText = target.text
         Content = BaseContentText[:300] + (BaseContentText[300:] and '..')
-        await ctx.send(f"{Content}")
+        Heck = Content.find("Special Containment")
+
+        #TODO So because I like colours we're going to make the embed colour based off the object class
+        await ctx.send(f"{Heck}")
         scpEM = discord.Embed(
             titlle=f"{target.title}",
-            url=f"{target.url}"
+            url=f"{target.url}",
+            description=f"{Content}"
 
         )
 
