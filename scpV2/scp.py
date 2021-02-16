21adc3c8e519da7302c3490b3424bb448995419b
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-16 22:31:01 +0000

    Update scp.py

--- scpV2/scp.py
+++ scpV2/scp.py
@@ -60,6 +60,7 @@ class SCP(commands.Cog):
         #Then, we'll attempt to grab the Special Containment Procedures in a similar manner.
             try:
                 SpeConProStr = Content[Content.find("Special Containment Procedures"):Content.find("Description")]
+                ContainmentToEmbed = SpeConProStr[3:]
                 #Instead of splitting like last time, this time we'll join off a split for the fun of it.
             except:
                 ContainmentToEmbed = "Couldn't obtain the Containment Procedure..."
