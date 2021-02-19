f1a5705c77e09e2b6f3ad8e36ea6bc73e17d6729
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-19 19:40:42 +0000

    Update warnings.py

--- warnings/warnings.py
+++ warnings/warnings.py
@@ -566,6 +566,7 @@ class Warnings(commands.Cog):
         count = 1
 
         dab = menus.MenuPages(source=self.WarningsMenu(pages=["my","wife","left"], users=ctx.author), clear_reactions_after=True)
+        await dab.start()
         member_settings = self.config.member(user)
         async with member_settings.warnings() as user_warnings:
             if not user_warnings.keys():  # no warnings for the user
