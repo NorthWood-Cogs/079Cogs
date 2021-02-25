69c7f65a121823b36eed134b16e1bf1fa55f7deb
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-02-25 13:17:55 +0000

    I hate SCP 2521 (#21)
    Special Cases arefummmmmmmmmmnnnnnnnnnnnnnnn

--- scp/scp.py
+++ scp/scp.py
@@ -1,5 +1,5 @@
 import discord
-from discord.errors import Forbidden
+from discord.errors import Forbidden, HTTPException
 import pyscp # Installed On Cog install, using https://github.com/NorthWood-Cogs/pyscp
 import re
 import asyncio
@@ -52,54 +52,65 @@ class SCP(commands.Cog):
         target = self.SCPWiki(f'scp-{scpID}')  #pyscp handles the rest
         Content = target.text
         #So by using string finds, we're gonna pick out the first "block" of the article
-        ObjectClassFinder = await target.source #I hate their templates, this is the workaround.
-        try:
-            #now, the problem with our method is that it creates A LOT of ways for it to go wrong. so lets prepare for that.
-            #We'll firstly gleam it for an object Class - Safe, Euclid, etc... and also the corresponding Colour.
-            #Sorta Cute you'd think its easy. The issue with the wiki is one of inconsistent formatting and names - so we'll need to encounter for banner styles where possible
-            #Now that ObjectClassFinder is Making sense, eh?
-            try: # and so, i bring to you the triple-try-that-probably-doesn't-need-to-be-this-way-loop. Run.
+        ObjectClassFinder = await target.source
+        if scpID == "2521": # Fucking Edge cases
+            em = discord.Embed(
+                title="●●|●●●●●|●●|●",
+                url="http://www.scpwiki.com/scp-2521",
+                color=0xe74c3c
+            )
+            em.set_image(url="http://scp-wiki.wdfiles.com/local--files/scp-2521/scp_number.jpg")
+            try:
+                await ctx.send(embed=em)
+            except:
+                await ctx.send("I have no embed perms.")
+        else:
+            try:
                 try:
                     ObjectCLStr = Content[Content.find("Object Class"):]
                     ObjectSplit = ObjectCLStr.split() #This will (try) to find a string
                     OBJCL = ObjectSplit[2]
+                    ClassColour = await self.ColourPicker(OBJCL)
                 except:
-                    OBJCL = re.search("/safe|euclid|keter|thaumiel|explained|neutralized/im", ObjectClassFinder).group()
+                    try:
+                        OBJCL = re.search("/safe|euclid|keter|thaumiel|explained|neutralized/im", ObjectClassFinder).group()
+                        ClassColour = await self.ColourPicker(OBJCL)
                     # the less neat way...
-                ClassColour = await self.ColourPicker(OBJCL)
+                    except:
+                        OBJCL = "Failed to Obtain Object Class..."
+                        ClassColour = 0x99aab5 
+            #Then, we'll attempt to grab the Special Containment Procedures in a similar manner.
+                try:
+                    SpeConProStr = Content[Content.find("Special Containment Procedures"):Content.find("Description")]
+                    ContainmentInfo = " ".join(SpeConProStr.split(" ")[3:])
+                    ContainmentToEmbed = ContainmentInfo[:1000] + (ContainmentInfo[1000:] and '...')
+                    #Instead of splitting like last time, this time we'll join off a split for the fun of it.
+                except:
+                    ContainmentToEmbed = "Couldn't obtain the Containment Procedure..."
+
+                errors = ""
             except:
-                OBJCL = "Failed to Obtain Object Class..."
-                ClassColour = 0x99aab5 
-        #Then, we'll attempt to grab the Special Containment Procedures in a similar manner.
+                errors = "There was some trouble obtaining some information. Typically, this is due to an archive warning - the Link should work fine to open the real article."
+                ClassColour = self.ColourPicker("Keked") #Greyple in case it all goes wrong
+
+            scpEM = discord.Embed(
+                title=f"{target.title}",
+                url=f"{target.url}", #We're not really including a lot in the base embed (NOTE to self do I want a footer?) 
+                colour=ClassColour,     # Since we want custom fields for the formatting.
+            )
+            try: #as all this is, technically, not required, so it gets its own try loop. THE ORDER HERE IS IMPORTANT!
+                OBJCCL = OBJCL.capitalize()
+                scpEM.add_field(name="Object Class",value=f"{OBJCCL}",inline=False)
+                scpEM.add_field(name="Special Containment Procedures", value=f"{ContainmentToEmbed}",inline=False)
+                scpEM.set_thumbnail(url=target.images[0]) #THUMBNAIL must ALWAYS be last, as not every page has an image attached
+            except:
+                pass
             try:
-                SpeConProStr = Content[Content.find("Special Containment Procedures"):Content.find("Description")]
-                ContainmentInfo = " ".join(SpeConProStr.split(" ")[3:])
-                ContainmentToEmbed = ContainmentInfo[:1000] + (ContainmentInfo[1000:] and '...')
-                #Instead of splitting like last time, this time we'll join off a split for the fun of it.
-            except:
-                ContainmentToEmbed = "Couldn't obtain the Containment Procedure..."
-
-            errors = ""
-        except:
-            errors = "There was some trouble obtaining some information. Typically, this is due to an archive warning - the Link should work fine to open the real article."
-            ClassColour = self.ColourPicker("Keked") #Greyple in case it all goes wrong
-
-        scpEM = discord.Embed(
-            title=f"{target.title}",
-            url=f"{target.url}", #We're not really including a lot in the base embed (NOTE to self do I want a footer?) 
-            colour=ClassColour,     # Since we want custom fields for the formatting.
-        )
-        try: #as all this is, technically, not required, so it gets its own try loop. THE ORDER HERE IS IMPORTANT!
-            OBJCCL = OBJCL.capitalize()
-            scpEM.add_field(name="Object Class",value=f"{OBJCCL}",inline=False)
-            scpEM.add_field(name="Special Containment Procedures", value=f"{ContainmentToEmbed}",inline=False)
-            scpEM.set_thumbnail(url=target.images[0]) #THUMBNAIL must ALWAYS be last, as not every page has an image attached
-        except:
-            pass
-        try:
-            await ctx.send(f"{errors}",embed=scpEM)
-        except Exception(Forbidden):
-            await ctx.send("I can't send embeds here!")
+                await ctx.send(f"{errors}",embed=scpEM)
+            except HTTPException(Forbidden):
+                try:
+                    await ctx.send("I can't send embeds here! Probably")
+                except: pass
 
         
 
