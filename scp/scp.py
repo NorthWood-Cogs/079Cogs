2762b0bb03ca11ae4801585ae0f3adb8c5c05f9a
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-03-28 14:37:24 +0100

    Update scp.py

--- scp/scp.py
+++ scp/scp.py
@@ -23,7 +23,7 @@ class SCP(commands.Cog):
         )    
 
     @commands.command(name="scp")
-    async def _scp(self, ctx, *, scp):
+    async def _scp(self, ctx, *, scp: str):
         await self.CromRequest(scp=scp)
 
 
