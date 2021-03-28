6ae9576d73fb8404fc2afa9a46a7b0d68b011c39
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-03-28 23:49:10 +0100

    More error catching

--- scp/scp.py
+++ scp/scp.py
@@ -21,7 +21,7 @@ class SCP(commands.Cog):
     async def _scp(self, ctx, *, scp: str):
         """Attempts to search for an SCP. Denote them with `-ex` or `-j` to go for explained/joke scp's (and others!)
             Searching by ID is almost always ideal, though names will work in most cases - Pascal Case is ideal.""" 
-        if len(scp) <= 2 and scp.isdecimal():
+        if len(scp) <= 2 and scp.isdecimal() and len(scp)[0] != "-":
             scpID = scp.replace(" ", "")
             scpToSearch = (f"{scpID.zfill(3)}")
         else:
