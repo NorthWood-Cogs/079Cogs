a9659427aaa4e73c1272fecbec533b0a32b28de7
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-03-28 22:58:12 +0100

    Update scp.py

--- scp/scp.py
+++ scp/scp.py
@@ -24,7 +24,7 @@ class SCP(commands.Cog):
 
     
     async def CromRequest(self, ctx, scp):
-        UA = (f"Redbot Cog {discord.ClientUser.name} UID: {discord.ClientUser.name}")
+        UA = (f"Redbot Cog {discord.ClientUser.name} UID: {discord.ClientUser.name}") # The lad asked I made sure to identify the bot in the Session via User agents.
         async with aiohttp.ClientSession(headers={'User-Agent': UA}) as session:
             Client = client.GraphQLClient(
                 endpoint = "https://api.crom.avn.sh/"
