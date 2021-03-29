d00f58b901c60b65b689bfda54941462c6b49ac8
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-03-29 12:15:49 +0100

    Moving Cogs to other repo

--- moreowners/__init__.py
+++
@@ -1,4 +0,0 @@
-from .moreowners import MoreOwners
-
-async def setup(bot):
-    bot.add_cog(MoreOwners(bot))
--- pingalert/__init__.py
+++
@@ -1,5 +0,0 @@
-from .pingalert import PingAlert
-
-
-def setup(bot):
-    bot.add_cog(PingAlert(bot))
