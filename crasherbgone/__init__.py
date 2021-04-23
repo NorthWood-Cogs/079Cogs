fbfbcd2cbe9f4b7fedbd660969441df724d2329e
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-04-24 00:15:42 +0100

    bot

--- crasherbgone/__init__.py
+++ crasherbgone/__init__.py
@@ -1,6 +1,6 @@
 from .crasherbgone import CrasherBGone
 
 async def setup(bot):
-    cog = CrasherBGone()
+    cog = CrasherBGone(bot)
     await cog.initialize()
     bot.add_cog(CrasherBGone(bot))
