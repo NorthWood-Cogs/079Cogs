f751d6512393008534b4468511eb1cfd1d4f2208
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-04-24 01:47:48 +0100

    Detection trial run

--- crasherbgone/crasherbgone.py
+++ crasherbgone/crasherbgone.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+import os
 import re
+import subprocess
+import aiohttp
 import discord
 import asyncio
 from discord.ext.commands.core import guild_only
@@ -8,6 +12,9 @@ from redbot.core.bot import Red
 from typing import Union
 from redbot.core import modlog
 from redbot.core.modlog import register_casetype, register_casetypes
+from subprocess import Popen # its just os with added memes
+from redbot.core.data_manager import cog_data_path #For image storage
+import secrets
 
 
 
@@ -69,9 +76,6 @@ class CrasherBGone(commands.Cog):
         else:
             await ChannelConfig.enabled.set(True)
             await ctx.reply("Crash Video checking is enabled within {Chann}".format(Chann=Chan))
-  
-
-
 
     @commands.group()
     @guild_only()
@@ -174,3 +178,67 @@ class CrasherBGone(commands.Cog):
                         case = await modlog.create_case(
                             self.bot, message.channel.guild, message.created_at, action_type="videoclr", user=message.author,
                             moderator=self.bot.user, reason="`giant.gfycat` link that ends in mp4 - I'd recommend checking the link yourself. Content: `{link}`".format(link=message.content))
+                        await message.channel.send("Possible Crash Gif deleted, logged, and reported.")
+                        await message.delete()
+
+                    if LogMode == "ChannelLog":
+                        logchannel = self.bot.get_channel(GuildSettingsDict["logchannel"])
+                        await logchannel.send("NOTICE - {twat} has possibly posted a discord crashing gif/mp4.".format(twat=message.user.mention))
+                        await logchannel.send("Message Content - {twats_words}".format(twats_words=message.content))
+                        await message.delete()
+                        await message.channel.send("Possible Crash Gif deleted, logged, and reported.")
+                        await message.delete()
+                    else:
+                        await message.channel.send("possible Crash Gif deleted..")
+                        await message.delete()
+            #And the next parts feature a bit of stealing from Aiks attempts to do something similar. Hopefully we have more success.
+            if message.content.startswith("https://cdn.discordapp.com/attachments/"):
+                maybe_url = message.content.split()[0]
+                if maybe_url.endswith(".mp4") or maybe_url.endswith(".gif"):
+                    await self._video_checker(maybe_url)
+        if message.attachments != None: # and now the real fun begins.
+            pass
+    
+
+
+
+    async def _video_checker(self, inputfile): #Yeah Aikaternas File downloader seems to be as good as I'm gonna get, really. mix it in with Popen for bonus points.
+        r = secrets.token_hex(6)
+        CogPath = f"{cog_data_path(self)}/"
+        file_name = f"temp_vid{r}.mp4"
+        file_file = f"{CogPath}{file_name}"
+        start_frame_file = f"{CogPath}start_frame{r}.jpg"
+        end_frame_file = f"{CogPath}end_frame{r}.jpg"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(inputfile) as resp:
+                data = resp.content.read()
+                with open(file_file, "wb+") as g:
+                    g.write(data)
+        try:
+            resultSFF = self.bot.loop.run_in_executor(None, subprocess.call(["ffmpeg", "-i", file_file, "-vframes 1", "-q:v 1", f"{start_frame_file}"], timeout=60))
+            resultEFF = self.bot.loop.run_in_executor(None, subprocess.call(["ffmpeg", "-sseof -3", "-i", file_file, "-update 1", "-q:v 1", f"{end_frame_file}"], timeout=60))
+        except subprocess.TimeoutExpired:
+            os.remove(file_file)
+            try:
+                os.remove(start_frame_file)
+            except FileNotFoundError:
+                pass
+            try:
+                os.remove(end_frame_file)
+            except FileNotFoundError:
+                pass
+            return "ERROR"
+        # OK so now that we have our two images, its time to probe
+        resultEFF.cancel()
+        resultSFF.cancel() # make sure they're dead.
+        SFFOut = None
+        EFFOut = None
+        try:
+            probeSFF = self.bot.loop.run_in_executor(None, subprocess.call(["ffprobe", "-i", start_frame_file], stdout=SFFOut, timeout=60))
+            probeEFF = self.bot.loop.run_in_executor(None, subprocess.call(["ffprobe", "-i", end_frame_file], stdout=EFFOut, timeout=60))
+            print(SFFOut)
+            print(EFFOut)
+        except:
+            pass
+
