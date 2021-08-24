bce01baa3079be8eb28d148d3e0815d948ad3ff2
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2021-08-24 19:23:20 +0100

    Scp my wife left me (#30)
    added a random function, and grabbed some more error handling and shoved it down the bots throat with one hand while holding the one free arm it has with another and watched as the light faded from its eyes. If only...

--- scp/scp.py
+++ scp/scp.py
@@ -23,6 +23,12 @@ class SCP(commands.Cog):
     async def _scp(self, ctx, *, scp: str):
         """Attempts to search for an SCP. Denote them with `-ex` or `-j` to go for explained/joke scp's (and others!)
             Searching by ID is almost always ideal, though names will work in most cases - Pascal Case is ideal.""" 
+        if scp.lower() == "random":
+            lol = random.randint(1, 6999)
+            #print(str(lol))
+            emb = await self.CromRequest(ctx, scp=str(lol), BotSelf=ctx.guild.me)
+            await ctx.send(embed=emb)
+        
         if scp[0] == "-":
             em = discord.Embed(
                 title="Error!",
@@ -45,7 +51,7 @@ class SCP(commands.Cog):
             em.add_field(name="Help", value="We're trapped in his basement, help!", inline=False)
             em.set_thumbnail(url="https://cdn.discordapp.com/attachments/681599779242770444/817006021276336128/HubS.png")
             await ctx.send(embed=em)
-        else:
+        elif scp.lower() != "random":
             emb = await self.CromRequest(ctx, scpToSearch, BotSelf=theBot)
             await ctx.send(embed=emb)
 
@@ -97,8 +103,14 @@ class SCP(commands.Cog):
                     description="This isnt a valid SCP name. Try its Article number, or its formal name, if it has one."
                 )
                 return em
-
-            emName = (f"{coreJson['alternateTitles'][0]['title']}") # Expects to be a strin, is the 'name' e.g. SCP 079's name is "Old AI"
+            try:
+                emName = (f"{coreJson['alternateTitles'][0]['title']}") # Expects to be a strin, is the 'name' e.g. SCP 079's name is "Old AI"
+            except:
+                em = discord.Embed(
+                    title="Error!",
+                    description="This isnt a valid SCP name. Try its Article number, or its formal name, if it has one."
+                )
+                return em
             if emTitle != emName:
                 emTrueTitle = (f"{emTitle} - {emName}")
             else:
