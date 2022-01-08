bc6586dd0c81e3ac08d5e28758b3142d38aeef5a
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2022-01-08 22:23:16 +0000

    fuck it requests time

--- qrbeeper/notquitehelping.py
+++ qrbeeper/notquitehelping.py
@@ -1,22 +1,20 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
-import aiohttp
-import asyncio
+import requests
 from io import BytesIO
 import discord
 
 
 async def QRScanner(message: discord.Message, data: str):
     """Main Logic for actually handling the QR Codes. Actually quite simple."""
-    async with aiohttp.ClientSession() as sesh:
-        async with sesh.get(data) as resp:
-            #await message.channel.send(message.content)
-            #await message.channel.send(data)
-            decodedImage = decode(Image.open(BytesIO(resp.content)))
-            await message.channel.send(str(decodedImage))
-            AreWeThereYet = str(decodedImage[0])
-            QRURL = AreWeThereYet.partition("data=b'")[2].partition("',")[0]
-            TheRealQRURL = (f"`{QRURL}`")
+    response = requests.get(data)
+    #await message.channel.send(message.content)
+    #await message.channel.send(data)
+    decodedImage = decode(Image.open(BytesIO(response.content)))
+    await message.channel.send(str(decodedImage))
+    AreWeThereYet = str(decodedImage[0])
+    QRURL = AreWeThereYet.partition("data=b'")[2].partition("',")[0]
+    TheRealQRURL = (f"`{QRURL}`")
 
-            await message.channel.send(TheRealQRURL)
+    await message.channel.send(TheRealQRURL)
 
