6581ba5de142085547d3d5f4fcd68b7dfc5b6e6d
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2022-01-08 22:46:29 +0000

    Update notquitehelping.py

--- qrbeeper/notquitehelping.py
+++ qrbeeper/notquitehelping.py
@@ -3,11 +3,14 @@ from PIL import Image
 import requests
 from io import BytesIO
 import discord
-
+import re
 
 async def QRScanner(message: discord.Message, data: str):
     """Main Logic for actually handling the QR Codes. Actually quite simple."""
-    response = discord.asset.read(data)
+    if discord.Asset(data):
+        response = discord.Asset.read(data)
+    else:
+        valid = re.search("(?i)http(?::\/\/|s:\/\/).*(?:twimg|png|jpg|jpeg|)", data)
     decodedImage = decode(Image.open(BytesIO(response.content)))
     AreWeThereYet = str(decodedImage[0])
     QRURL = AreWeThereYet.partition("data=b'")[2].partition("',")[0]
