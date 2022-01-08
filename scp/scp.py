8d841e352223c2c83de9d229a0d2ae96714e43ec
Danstr5544 <73479857+Danstr5544@users.noreply.github.com>
2022-01-08 23:03:32 +0000

    Update scp.py (#33)
    * Update scp.py
    
    * changing a single charactar to wake CodeQL up

--- scp/scp.py
+++ scp/scp.py
@@ -71,7 +71,7 @@ class SCP(commands.Cog):
             else:
                 targetSCP = str(scp).title()
                 
-            #The query itself. If you ever need to update this(Which you shouldn't, thats my problem) just add it into the existing layout.
+            #The query itself. If you ever need to update this(Which you shouldn't, thats my problem) just add it into the existing layout
             CromQuery = client.GraphQLRequest("""  
             {{
             searchPages(query: "{targetScp}")
@@ -106,7 +106,8 @@ class SCP(commands.Cog):
                 )
                 return em
             try:
-                emName = (f"{coreJson['alternateTitles'][0]['title']}") # Expects to be a strin, is the 'name' e.g. SCP 079's name is "Old AI"
+                emNameToGo = (f"{coreJson['alternateTitles'][0]['title']}") # Expects to be a strin, is the 'name' e.g. SCP 079's name is "Old AI"
+                emName = (emNameToGo[:200] + "...") if len(emNameToGo) > 200 else emNameToGo
             except:
                 em = discord.Embed(
                     title="Error!",
@@ -114,7 +115,8 @@ class SCP(commands.Cog):
                 )
                 return em
             if emTitle != emName:
-                emTrueTitle = (f"{emTitle} - {emName}")
+                emTT = (f"{emTitle} - {emName}")
+                emTrueTitle = (emTT[:200] + "...") if len(emTT) > 200 else emTT
             else:
                 emTrueTitle = emTitle
             
